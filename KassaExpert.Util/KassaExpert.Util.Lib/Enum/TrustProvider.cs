09b09071ebdbdadf899b6254fa4e6ca0af84d46d
MS <simon@mauracher.cc>
2021-01-27 18:18:57 +0100

    added trustprovider

---
+++ KassaExpert.Util/KassaExpert.Util.Lib/Enum/TrustProvider.cs
@@ -0,0 +1,18 @@
+﻿using Enum.Ext;
+
+namespace KassaExpert.Util.Lib.Enum
+{
+    public sealed class TrustProvider : TypeSafeEnum<TrustProvider, int>
+    {
+        public static readonly TrustProvider A_Trust = new TrustProvider(1, "AT1");
+        public static readonly TrustProvider GlobalTrust = new TrustProvider(2, "AT2");
+        public static readonly TrustProvider TEST = new TrustProvider(9, "AT9");
+
+        private TrustProvider(int id, string abbreviation) : base(id)
+        {
+            Abbreviation = abbreviation;
+        }
+
+        public string Abbreviation { get; }
+    }
+}
