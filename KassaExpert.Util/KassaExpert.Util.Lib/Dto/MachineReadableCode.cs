c0183c0ddb72ad8bdb90acc80aa2cd37b4073a17
MS <simon@mauracher.cc>
2021-01-27 18:19:32 +0100

    added machine-readable-code

---
+++ KassaExpert.Util/KassaExpert.Util.Lib/Dto/MachineReadableCode.cs
@@ -0,0 +1,142 @@
+﻿using KassaExpert.Util.Lib.Date;
+using KassaExpert.Util.Lib.Date.Impl;
+using KassaExpert.Util.Lib.Enum;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace KassaExpert.Util.Lib.Dto
+{
+    public sealed class MachineReadableCode
+    {
+        public MachineReadableCode(string formatedCode)
+        {
+        }
+
+        public MachineReadableCode(TrustProvider trustProvider, string cashboxId,
+            string receiptNumber, DateTime austiraDate,
+            decimal amountTax20, decimal amountTax10, decimal amountTax13, decimal amountTax0, decimal amountTax19,
+            string encryptedRevenueCounter, string certificateSerialNumber, string signaturePreviousReceipt, string? signature = null)
+        {
+            TrustProvider = trustProvider;
+            CashboxId = cashboxId;
+            ReceiptNumber = receiptNumber;
+            AustiraDate = austiraDate;
+            AmountTax20 = amountTax20;
+            AmountTax10 = amountTax10;
+            AmountTax13 = amountTax13;
+            AmountTax0 = amountTax0;
+            AmountTax19 = amountTax19;
+            EncryptedRevenueCounter = encryptedRevenueCounter;
+            CertificateSerialNumber = certificateSerialNumber;
+            SignaturePreviousReceipt = signaturePreviousReceipt;
+            Signature = signature;
+        }
+
+        public TrustProvider TrustProvider { get; }
+
+        /// <summary>
+        /// Kassen-ID
+        /// MUST BE UTF-8 <see cref="Validation.IValidation.IsUtf8String(string)"/>
+        /// </summary>
+        public string CashboxId { get; }
+
+        /// <summary>
+        /// Belegnummer
+        /// </summary>
+        public string ReceiptNumber { get; }
+
+        /// <summary>
+        /// Beleg-Datum-Uhrzeit
+        /// </summary>
+        public DateTime AustiraDate { get; }
+
+        /// <summary>
+        /// Betrag-Satz-Normal
+        /// </summary>
+        public decimal AmountTax20 { get; }
+
+        /// <summary>
+        /// Betrag-Satz-Ermaessigt-1
+        /// </summary>
+        public decimal AmountTax10 { get; }
+
+        /// <summary>
+        /// Betrag-Satz-Ermaessigt-2
+        /// </summary>
+        public decimal AmountTax13 { get; }
+
+        /// <summary>
+        /// Betrag-Satz-Null
+        /// </summary>
+        public decimal AmountTax0 { get; }
+
+        /// <summary>
+        /// Betrag-Satz-Besonders
+        /// </summary>
+        public decimal AmountTax19 { get; }
+
+        /// <summary>
+        /// Stand-Umsatz-Zaehler-AES256-ICM
+        /// </summary>
+        public string EncryptedRevenueCounter { get; }
+
+        /// <summary>
+        /// Zertifikat-Seriennummer [HEX ohne prefix 0x] <see cref="Validation.IValidation.IsValidHexSerial(string)"/>
+        /// </summary>
+        public string CertificateSerialNumber { get; }
+
+        /// <summary>
+        /// Sig-Voriger-Beleg
+        /// </summary>
+        public string SignaturePreviousReceipt { get; }
+
+        public string? Signature { get; }
+
+        public string GetCode()
+        {
+            IDate dateConverter = new DefaultDate();
+
+            var sb = new StringBuilder();
+
+            sb.Append("_R1-");
+            sb.Append(TrustProvider.Abbreviation);
+            sb.Append('_');
+            sb.Append(CashboxId);
+            sb.Append('_');
+            sb.Append(ReceiptNumber);
+            sb.Append('_');
+            sb.Append(dateConverter.FormatDate(AustiraDate));
+            sb.Append('_');
+            sb.Append(FormatAmount(AmountTax20));
+            sb.Append('_');
+            sb.Append(FormatAmount(AmountTax10));
+            sb.Append('_');
+            sb.Append(FormatAmount(AmountTax13));
+            sb.Append('_');
+            sb.Append(FormatAmount(AmountTax0));
+            sb.Append('_');
+            sb.Append(FormatAmount(AmountTax19));
+            sb.Append('_');
+            sb.Append(EncryptedRevenueCounter);
+            sb.Append('_');
+            sb.Append(CertificateSerialNumber);
+            sb.Append('_');
+            sb.Append(SignaturePreviousReceipt);
+
+            if (Signature is not null)
+            {
+                sb.Append('_');
+                sb.Append(Signature!);
+            }
+
+            return sb.ToString();
+        }
+
+        internal static string FormatAmount(decimal amount)
+        {
+            return Math.Round(amount, 2).ToString("0.00", CultureInfo.InvariantCulture).Replace('.', ',');
+        }
+    }
+}
---
+++ KassaExpert.Util/KassaExpert.Util.LibTest/DtoTests/TestMachineReadableCode.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using KassaExpert.Util.Lib.Dto;
+using KassaExpert.Util.Lib.Enum;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KassaExpert.Util.LibTest.DtoTests
+{
+    [TestFixture]
+    public class TestMachineReadableCode
+    {
+        [Test]
+        public void TestGetCode()
+        {
+            var tmp = new MachineReadableCode(TrustProvider.A_Trust, "DEMO-CASH-BOX524", "366585AB",
+                new DateTime(2015, 12, 17, 11, 23, 43), 10.50m, 0.0m, 0.0m, 0.78m, 0.0m, "Q+dTEnSc", "245abcde", "OJ16FcqeA7s");
+
+            var resultCode = tmp.GetCode();
+
+            resultCode.Should().Be("_R1-AT1_DEMO-CASH-BOX524_366585AB_2015-12-17T11:23:43_10,50_0,00_0,00_0,78_0,00_Q+dTEnSc_245abcde_OJ16FcqeA7s");
+        }
+    }
+}
